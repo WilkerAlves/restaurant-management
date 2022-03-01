75b63c55fc9b902b0e5c4a160a43cd3857a71eda
Wilker Alves <wilkeralves97@gmail.com>
2022-03-01 17:05:38 -0300

    corrigindo models

--- models/foodModel.go
+++ models/foodModel.go
@@ -8,11 +8,11 @@ import (
 
 type Food struct {
 	Id        primitive.ObjectID `bson:"_id"`
+	FoodId    string             `json:"food_id"`
 	Name      *string            `json:"name" validate:"required, mim=2, max=100"`
 	Price     *float64           `json:"price" validate:"required"`
 	FoodImage *string            `json:"food_image" validate:"required"`
+	MenuId    *string            `json:"menu_id" validate:"required"`
 	CreatedAt time.Time          `json:"created_at"`
 	UpdatedAt time.Time          `json:"updated_at"`
-	FoodId    string             `json:"food_id"`
-	MenuId    *string            `json:"menu_id" validate:"required"`
 }
--- models/invoiceModel.go
+++ models/invoiceModel.go
@@ -11,7 +11,7 @@ type Invoice struct {
 	InvoiceId      string             `json:"invoice_id"`
 	OrderId        string             `json:"order_id"`
 	PaymentMethod  *string            `json:"payment_method" validate:"eq=CARD|eq=CASH|eq="`
-	PaymentStatus  *string            `json:"payment_status"  validate:"required,eq=PENDING|eq=PAID"`
+	PaymentStatus  *string            `json:"payment_status" validate:"required,eq=PENDING|eq=PAID"`
 	PaymentDueDate time.Time          `json:"payment_due_date"`
 	CreatedAt      time.Time          `json:"created_at"`
 	UpdatedAt      time.Time          `json:"updated_at"`
